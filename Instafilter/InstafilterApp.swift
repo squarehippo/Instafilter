510d11ebffeb7da497d6f67e8d89bff1350ec197
Brian Wilson <squarehippobills@gmail.com>
2021-06-16 15:58:52 -0400

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Brian Wilson on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

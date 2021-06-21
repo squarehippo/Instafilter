719afcbe0b9ac661f2881f91705967a619674aaf
Brian Wilson <squarehippobills@gmail.com>
2021-06-21 12:56:36 -0400

    Completed app with challenge 1 & 2...ish

---
+++ Instafilter/ImageSaver.swift
@@ -0,0 +1,26 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Brian Wilson on 6/21/21.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
